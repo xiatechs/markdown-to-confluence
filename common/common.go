82138c814de543bed89a076e29d80c073be2f9fb
tbal999 <53711814+tbal999@users.noreply.github.com>
2021-05-07 12:44:32 +0100

    Update common.go

--- common/common.go
+++ common/common.go
@@ -1,5 +1,13 @@
-// Package common is for storing common constants used in app
+// Package common is for storing common constants/vars used in app
 package common
 
-// ConfluenceBaseURL is the base URL for the confluence page you want to work with
-const ConfluenceBaseURL = "https://xiatech-markup.atlassian.net"
+var ConstantsHardCoded = false
+
+const (
+  // ConfluenceBaseURL is the base URL for the confluence page you want to work with
+  ConfluenceBaseURL = "https://xiatech-markup.atlassian.net"
+	confluenceUsernameEnv = "INPUT_CONFLUENCE_USERNAME"
+	confluenceAPIKeyEnv   = "INPUT_CONFLUENCE_API_KEY"
+	confluenceSpaceEnv    = "INPUT_CONFLUENCE_SPACE"
+	CnvsNotSetError       = "environment variable not set, please assign values for: "
+)
