ded4a21b57c3cc1c5d4cd9c89caa2ed321ca4c71
tbal999 <53711814+tbal999@users.noreply.github.com>
2021-05-07 13:44:26 +0100

    re-added tests accidentally removed

--- markdown/markdown_test.go
+++ markdown/markdown_test.go
@@ -50,3 +50,54 @@ This Action will trawl through a repository.`),
 		})
 	}
 }
+
+
+func TestParseMarkdown_HappyPath(t *testing.T) {
+	testContent := strings.NewReader(`
++++
+categories = ["Development", "Github Actions"]
+date = "2021-03-10"
+description = "A guide on how to use the markdown to confluence action"
+slug = "markdown-to-confluence-guide"
+title = "Markdown to Confluence Action Guide"
++++
+
+# Test Content 
+test description`)
+
+	expectOutput := &markdown.FileContents{
+		MetaData: map[string]interface{}{
+			"categories":  []interface{}{"Development", "Github Actions"},
+			"date":        "2021-03-10",
+			"description": "A guide on how to use the markdown to confluence action",
+			"slug":        "markdown-to-confluence-guide",
+			"title":       "Markdown to Confluence Action Guide",
+		},
+		Body: []byte(`<h1>Test Content</h1>
+<p>test description</p>
+`),
+	}
+
+	out, err := markdown.ParseMarkdown(testContent)
+	assert.Nil(t, err)
+	assert.Equal(t, out, expectOutput)
+}
+
+func TestParseMarkdown_MalformedFrontMatter(t *testing.T) {
+	testContent := strings.NewReader(`
+	+++
+	badFrontMatter = 253svsasrg
+	categories = ["Development", "Github Actions"]
+	date = "2021-03-10"
+	description = "A guide on how to use the markdown to confluence action"
+	slug = "markdown-to-confluence-guide"
+	title = "Markdown to Confluence Action Guide"
+	+++
+	
+	# Test Content 
+	test description`)
+
+	_, err := markdown.ParseMarkdown(testContent)
+	assert.NotNil(t, err)
+}
+
