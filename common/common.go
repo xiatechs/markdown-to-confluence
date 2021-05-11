2b33864e463aa5e95252a607d584cd03e2171fa4
tbal999 <53711814+tbal999@users.noreply.github.com>
2021-05-11 16:27:48 +0100

    Update common.go

--- common/common.go
+++ common/common.go
@@ -2,23 +2,22 @@
 //nolint:all // text in here could be long but on purpose i.e a long URL
 package common
 
-// ConstantsHardCoded - set to true if you are testing this in
-// UAT environment & want to edit constants to be actual API parameters
-var ConstantsHardCoded = false
-
-const (
+var (
 	// ConfluenceBaseURL is the base URL for the confluence page you want to work with
 	ConfluenceBaseURL = "https://xiatech-markup.atlassian.net"
 
-	// ConfluenceUsernameEnv is to collect external env var INPUT_CONFLUENCE_USERNAME
-	ConfluenceUsernameEnv = "INPUT_CONFLUENCE_USERNAME"
+	// ConfluenceUsername is to collect external arg for confluence username
+	ConfluenceUsername = "INPUT_CONFLUENCE_USERNAME"
 
-	// ConfluenceAPIKeyEnv is to collect external env var INPUT_CONFLUENCE_API_KEY
-	ConfluenceAPIKeyEnv = "INPUT_CONFLUENCE_API_KEY"
+	// ConfluenceAPIKey is to collect external arg for api key
+	ConfluenceAPIKey = "INPUT_CONFLUENCE_API_KEY"
 
-	// ConfluenceSpaceEnv is to collect external env var INPUT_CONFLUENCE_SPACE
-	ConfluenceSpaceEnv = "INPUT_CONFLUENCE_SPACE"
+	// ConfluenceSpace is to collect external arg for confluence space
+	ConfluenceSpace = "INPUT_CONFLUENCE_SPACE"
 
-	// EnvsNotSetError is template env var not set error
-	EnvsNotSetError = "environment variable not set, please assign values for: "
+	// EnvsNotSetError is template env arg not set error
+	EnvsNotSetError = "args not set, please assign values for: "
+
+	// ProjectPathEnv is to collect external arg for project path
+	ProjectPathEnv = "./"
 )
