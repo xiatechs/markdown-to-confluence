f9ba4fdb1b3524603dd6dd2c16ae8573fda4f21e
tbal999 <53711814+tbal999@users.noreply.github.com>
2021-05-11 16:27:27 +0100

    Update main.go

--- main.go
+++ main.go
@@ -3,28 +3,41 @@ package main
 import (
 	"log"
 	"os"
+	"strings"
 
+	"github.com/xiatechs/markdown-to-confluence/common"
 	"github.com/xiatechs/markdown-to-confluence/confluence"
 	"github.com/xiatechs/markdown-to-confluence/node"
 )
 
-const projectPathEnv = "PROJECT_PATH"
-const defaultProjectPath = "./"
+func setArgs() bool {
+	var argLength = 4
+
+	if len(os.Args) > 1 {
+		vars := strings.Split(os.Args[1], "_")
+
+		if len(vars) == argLength {
+			common.ConfluenceAPIKey = vars[0]
+			common.ConfluenceSpace = vars[1]
+			common.ConfluenceUsername = vars[2]
+			common.ProjectPathEnv = vars[3]
+
+			return true
+		}
+	}
+
+	log.Println("usage: app apikey_space_username_path")
+
+	return false
+}
 
 func main() {
-	root := node.Node{}
+	if setArgs() {
+		root := node.Node{}
 
-	if client, err := confluence.CreateAPIClient(); err != nil {
-		log.Println(err)
-	} else {
-		projectPath, exists := os.LookupEnv(projectPathEnv)
-		if !exists {
-			log.Printf("Environment variable not set for %s", projectPathEnv)
-
-			projectPath = defaultProjectPath
-		}
-
-		if root.Start(projectPath, client) {
+		if client, err := confluence.CreateAPIClient(); err != nil {
+			log.Println(err)
+		} else if root.Start(common.ProjectPathEnv, client) {
 			root.Delete()
 		}
 	}
