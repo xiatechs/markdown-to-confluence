f6ba2a82f754b1ecad17bc6bcdb5002c5e049615
tbal999 <53711814+tbal999@users.noreply.github.com>
2021-05-25 13:17:35 +0100

    Update common.go

--- common/common.go
+++ common/common.go
@@ -1,22 +1,24 @@
 // Package common is for storing common constants/vars used in app
 package common
 
-var (
-	// ConfluenceBaseURL is the base URL for the confluence page you want to work with
-	ConfluenceBaseURL = "https://xiatech-markup.atlassian.net"
-
-	// ConfluenceUsername is to collect external arg for confluence username
-	ConfluenceUsername = "INPUT_CONFLUENCE_USERNAME"
-
-	// ConfluenceAPIKey is to collect external arg for api key
-	ConfluenceAPIKey = "INPUT_CONFLUENCE_API_KEY"
-
-	// ConfluenceSpace is to collect external arg for confluence space
-	ConfluenceSpace = "INPUT_CONFLUENCE_SPACE"
-
+const (
+	// ConfluenceBaseURL is the base URL for the confluence page you want the API to connect to
+	ConfluenceBaseURL = "https://xiatech-markup.atlassian.net"	
+	
 	// EnvsNotSetError is template env arg not set error
 	EnvsNotSetError = "args not set, please assign values for: "
+)
+
+var (
+	// ConfluenceUsername is to collect external arg for confluence username
+	ConfluenceUsername string
+
+	// ConfluenceAPIKey is to collect external arg for api key
+	ConfluenceAPIKey string
+
+	// ConfluenceSpace is to collect external arg for confluence space
+	ConfluenceSpace string
 
 	// ProjectPathEnv is to collect external arg for project path
-	ProjectPathEnv = "./"
+	ProjectPathEnv = string
 )
