90f5c052acb319f220b4e805da8bf14be3318017
tbal999 <53711814+tbal999@users.noreply.github.com>
2021-07-05 17:29:21 +0100

    Update common.go

--- common/common.go
+++ common/common.go
@@ -3,8 +3,8 @@ package common
 
 var (
 	// ConfluenceBaseURL is the base URL for the confluence page you want the API to connect to
-	// by default it is https://xiatech-markup.atlassian.net but can be changed below
-	ConfluenceBaseURL = "https://xiatech-markup.atlassian.net"
+	// by default it is https://xiatech.atlassian.net but can be changed below
+	ConfluenceBaseURL = "https://xiatech.atlassian.net"
 	// ConfluenceUsername is to collect external arg for confluence username
 	ConfluenceUsername string
 
